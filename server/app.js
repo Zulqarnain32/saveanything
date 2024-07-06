098a2db3553d70ea15a081460e2a2ef8a9d826d6
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-07-05 20:58:57 -0700

    ready to deploy its backend

--- server/app.js
+++ server/app.js
@@ -2,14 +2,12 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const port = 5000
-// const cookieParser = require('cookie-parser')
 
 const mongoose = require("mongoose")
 const userRouter = require("./routes/auth")
 
 
 app.use(express.json())
-// app.use(cookieParser())
 app.use(cors({
     origin:['http://localhost:5173'],
     methods:['GET','POST'],
@@ -18,7 +16,8 @@ app.use(cors({
 }))
 app.use("/auth",userRouter)
 
-mongoose.connect("mongodb://127.0.0.1:27017/savedMethod")
+// mongoose.connect("mongodb://127.0.0.1:27017/savedMethod")
+mongoose.connect("mongodb+srv://zulqarnain:savedFriends@cluster0.u0lv4zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => {
     console.log("connected to database");
 }).catch(err => console.log("not connected"))
